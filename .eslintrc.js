7aa199b0ff0b31a4462dfb41f62ec80b08872788
litsynp <nocte55is@gmail.com>
2022-11-02 21:54:25 +0900

    chore: set up lint, prettier, typescript

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+const createConfig = require('@titicaca/eslint-config-triple/create-config')
+
+module.exports = createConfig({
+  type: 'frontend',
+  project: './tsconfig.json',
+  enableTypeCheck: true,
+  allowedNames: [],
+})
